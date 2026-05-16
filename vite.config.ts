5208928f7816500080c9bcc0caebd62cde46be62
VulumeCode <2590984+VulumeCode@users.noreply.github.com>
2026-05-16 19:53:49 +0200

    vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,46 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 import svgr from 'vite-plugin-svgr';
-
+import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   base: '/Blossom-Rivers/',
-  plugins: [svgr(), preact()],
+  plugins: [svgr(), preact()
+    ,
+  VitePWA({
+    includeAssets: ['hanafudaicon.svg'],
+    manifest: {
+      "name": "花川 - Blossom Rivers",
+      "short_name": "花川",
+      "description": "Blossom Rivers hanafuda game",
+      "display": "standalone",
+      "background_color": "#592116",
+      "theme_color": "#592116",
+      "categories": [
+        "games"
+      ],
+      "lang": "en",
+      "id": "com.vulumecode.blossomrivers",
+      "icons": [
+        {
+          src: '/Blossom-Rivers/hanafudaicon.svg',
+          sizes: '192x192',
+          "purpose": "any"
+        },
+        {
+          src: '/Blossom-Rivers/hanafudaicon.svg',
+          sizes: '512x512',
+          "purpose": "any"
+        },
+        {
+          "src": "/Blossom-Rivers/hanafudaicon.svg",
+          "sizes": "any",
+          "purpose": "any"
+        }
+      ],
+    }
+  })
+
+  ],
   server: {
     allowedHosts: ["tuf-vincent"]
   }
