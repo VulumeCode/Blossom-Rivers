2ebb9f2859066ccf6da2d69c79e27a827087d65f
VulumeCode <2590984+VulumeCode@users.noreply.github.com>
2026-07-11 17:15:01 +0200

    svg inner shadow

--- src/svg-filters.tsx
+++ src/svg-filters.tsx
@@ -25,6 +25,17 @@ export function SvgFilters() {
                     />
                     <feBlend in="flood" in2="SourceGraphic" mode="screen" />
                 </filter>
+                <filter
+                    id="inner-shadow"
+                    color-interpolation-filters="sRGB"
+                    primitiveUnits="objectBoundingBox"
+                >
+                    <feFlood flood-color="#610000" />
+                    <feComposite operator="out" in2="SourceGraphic" />
+                    <feMorphology operator="dilate" radius="0.035" />
+                    <feGaussianBlur stdDeviation="0.005" />
+                    <feComposite operator="atop" in2="SourceGraphic" />
+                </filter>
             </defs>
         </svg>
     );
